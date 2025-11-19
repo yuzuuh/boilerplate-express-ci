753ffbbdea99e24e9554f1fc54eb0fbf98118ecb
Yuzu <tatian.nico.cachito@gmail.com>
2025-11-19 15:13:56 -0300

    serve index.html file

--- myApp.js
+++ myApp.js
@@ -4,7 +4,7 @@ let app = express();
 console.log("Hello World");
 
 app.get("/", function (req, res) {
-res.send("Hello Express");
+  res.sendFile(__dirname + "/views/index.html");
 });
 
 module.exports = app;
