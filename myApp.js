730a70569b6d8aeb101ae47faccdca9017501542
Yuzu <tatian.nico.cachito@gmail.com>
2025-11-19 15:04:25 -0300

    Añadir ruta Hello World

--- myApp.js
+++ myApp.js
@@ -1,39 +1,8 @@
 let express = require('express');
 let app = express();
 
+app.get("/", function (req, res) {
+res.send("Hello World");
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
