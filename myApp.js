209ce1b9f0673e51998060e2744d6ecb9291a7cc
Yuzu <tatian.nico.cachito@gmail.com>
2025-11-19 15:17:42 -0300

    serve static assets

--- myApp.js
+++ myApp.js
@@ -3,6 +3,8 @@ let app = express();
 
 console.log("Hello World");
 
+app.use("/public", express.static(__dirname + "/public"));
+
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
